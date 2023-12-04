57c493db482aad401a61186c873b49e453507c36
hagar <hagarnabil7@gmail.com>
2023-12-04 11:55:28 +0200

    mock testing

---
+++ src/test/java/guru/springframework/recipeapp/controllers/IndexControllerTest.java
@@ -0,0 +1,34 @@
+package guru.springframework.recipeapp.controllers;
+
+import guru.springframework.recipeapp.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class IndexControllerTest {
+    @Mock
+    RecipeService recipeService;
+    @Mock
+    Model model;
+    IndexController controller;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        controller = new IndexController(recipeService);
+    }
+
+    @Test
+    void getIndex() {
+        String viewName = controller.getIndex(model);
+
+        assertEquals("index", viewName);
+        verify(recipeService, times(1)).getRecipes();
+        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+    }
+}
