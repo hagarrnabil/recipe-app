fd518c50fe681d7daf9e9f219d1aafbc108c7029
hagar <hagarnabil7@gmail.com>
2023-12-04 12:09:23 +0200

    integration testing

--- src/test/java/guru/springframework/recipeapp/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/recipeapp/controllers/IndexControllerTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,6 +25,12 @@ class IndexControllerTest {
         MockitoAnnotations.initMocks(this);
         controller = new IndexController(recipeService);
     }
+
+//    @Test
+//    void testMockMVC() {
+//        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+//        mockMvc.perform()
+//    }
 
     @Test
     void getIndex() {
